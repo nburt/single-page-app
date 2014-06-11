1690177244d39d12ef0bb6c11a01677f9c321b71
Nathanael Burt <nathanael.burt@gmail.com>
2014-06-11 13:47:51 -0600

    user can create a person and form is reset.
    Signed-off-by: Zach Klabunde <zbunde@gmail.com>

--- app/assets/javascripts/contact_manager_app.js
+++ app/assets/javascripts/contact_manager_app.js
@@ -2,9 +2,31 @@ window.ContactManagerApp = {
   initialize: function () {
     $.getJSON('/api/people').success(function (response) {
       var people = response._embedded.people;
-      $.each(people, function(index, person) {
+      $.each(people, function (index, person) {
         var div = JST['templates/display_people'](person);
-        $('main .container').append(div);
+        $('[data-container=people]').append(div);
+      });
+    });
+
+    var form = JST['templates/create_person'];
+    $('[data-container=main]').append(form);
+    $('.create-form').submit(function (event) {
+      var inputs = $(this).serializeArray();
+
+      var formParams = {};
+      $.each(inputs, function () {
+        formParams[this.name] = this.value;
+      });
+      event.preventDefault();
+      $.ajax({
+        type: "POST",
+        url: "/api/people",
+        dataType: "json",
+        data: JSON.stringify(formParams)
+      }).success(function (response) {
+        var div = JST['templates/display_people'](response);
+        $('.create-form input[type=text]').val("");
+        $('[data-container=people]').append(div);
       });
     });
   }
--- spec/features/contact_manager_spec.rb
+++ spec/features/contact_manager_spec.rb
@@ -1,16 +1,29 @@
 require 'rails_helper'
 
-feature 'The one-page contact manager app' do
+feature 'The one-page contact manager app', js: true do
 
   before do
     load "#{Rails.root}/db/seeds.rb"
   end
 
-  scenario 'The homepage loads', js: true do
+  scenario 'The homepage loads' do
     visit '/'
     expect(page).to have_title("Contact Manager")
     expect(page).to have_content("Joe Example")
     expect(page).to have_content("Edna Example")
+  end
+
+  scenario 'User can add a person and a div class dispaying person shows up' do
+    visit '/'
+    fill_in 'first_name', :with => 'hello'
+    fill_in 'last_name', :with => 'yoyo'
+    fill_in 'address', :with => '1234 street'
+    click_on 'Create'
+    expect(page).to have_content 'hello yoyo'
+    expect(page).to have_content '1234 street'
+    within 'form' do
+      expect(page).to_not have_content 'hello'
+    end
   end
 
 end
